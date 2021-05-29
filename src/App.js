911ab5836dbadefd128b1e34498ebd7b8db41e41
Gerardo Petitto <petitto.gerardo@yahoo.com>
2021-05-28 23:36:45 -0500

    Added input field, needs debugging

--- src/App.js
+++ src/App.js
@@ -12,11 +12,14 @@ import logo from "./assets/img/CrystalEthLogo.jpeg";
 import { BackgroundColorContext } from "./contexts/BackgroundColorContext";
 
 import Moralis from 'moralis';
+import { Web3 } from "moralis/lib/browser/Parse";
+import { EthereumEvents } from "moralis/lib/browser/MoralisWeb3";
 
 Moralis.initialize("DQ8U9WF2H28hQrmXxLPo7hs4OGnJGZgcrvlgdjFk");
 Moralis.serverURL = 'https://zd6oyblshpkc.Moralis.io:2053/server'
 
 const initialUser = Moralis.User.current();
+const contractAddress = '0x908e54F21c907AA493F81DB694e3eF6316454681';
 
 function App() {
   const [user, setUser] = useState(initialUser);
@@ -29,10 +32,22 @@ function App() {
     const user = await Moralis.authenticate();
     setUser(user);
   };
+
   const onLogout = () => {
     Moralis.User.logOut();
     setUser(null);
   };
+
+  const depositERC = async () => {
+    let amount = document.getElementById("user-deposit").value;
+    alert(amount);
+    window.web3 = await Moralis.Web3.enable();
+    let contractInstance = new Web3.eth.Contract(window.abi, contractAddress)
+    contractInstance.methods.deposit({value: amount, from: EthereumEvents.selectedAddress})
+    .on('receipt', function(receipt){
+      console.log(receipt);
+    });
+  }
 
   const toggleSidebar = () => {
     document.documentElement.classList.toggle("nav-open");
@@ -68,8 +83,10 @@ function App() {
                   // we don't want the Footer to be rendered on map page
                 // location.pathname === "/admin/maps" ? null : <Footer fluid />
                 }
+                <button onClick={onLogout}>Logout</button>;
+                <input type='number' id='user-deposit'></input>
+                <button onClick={depositERC}>Enter Deposit</button>
               </div>
-              <button onClick={onLogout}>Logout</button>;
             </div>
             <FixedPlugin bgColor={color} handleBgClick={changeColor} />
           </React.Fragment>
